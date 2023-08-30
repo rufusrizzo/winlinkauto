14c7f85751f64b93836940ca933c0471aeee4985
RileyC <rufus.rizzo78@gmail.com>
2023-08-30 13:59:25 -0400

    created the WL wed stuff

--- pat-sender-vhf.sh
+++ pat-sender-vhf.sh
@@ -1,3 +1,45 @@
 #!/bin/bash
+mycall=`pat env | grep MYCALL | awk -F "\"" '{print $2}'`
+[[ -z $1 ]] && wlw=n || wlw=$1
 
-printf "KF4EMZ, Riley, Waynesboro, VA (VHF)" | pat compose --from KF4EMZ --subject "Winlink Wednesday Check-In" KN4LQN riley@netandnix.net
+if [[ -z $2 && $wlw == "y" ]] 
+then
+        read -p "Enter the name you wish to use: " name
+else
+        name=$2
+fi
+if [[ -z $3 && $wlw == "y" ]] 
+then
+	echo "Location example: Waynesboro, VA"
+        read -p "Enter the location you wish to use: " location
+else
+        location=$3
+fi
+if [[ -z $2 && $wlw == "n" ]] 
+then
+        read -p "Enter the Subject you wish to use: " subject
+else
+        subject=$2
+fi
+if [[ -z $3 && $wlw == "n" ]] 
+then
+        read -p "Enter the message you wish to use: " message
+else
+        message=$3
+fi
+
+[[ -z $4 ]] || second_addr=$4
+
+
+
+if [[ $wlw == "y" ]]
+then
+	printf "$mycall, $name, $location (VHF)" | pat compose --from $mycall --subject "Winlink Wednesday Check-In" KN4LQN "$second_addr"
+exit
+fi
+
+if [[ $wlw == "n" ]]
+then
+	printf "$message `date`" | pat compose --from $mycall --subject "$subject" $mycall "$second_addr"
+exit
+fi
