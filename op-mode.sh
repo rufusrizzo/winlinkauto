b3559ad90293a2351534c28a5602b0503841ad86
RileyC <rufus.rizzo78@gmail.com>
2023-09-20 15:44:03 -0400

    working on service daemons, and added a tmux session to watch things

---
+++ op-mode.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#This script will create a tmux session to tail all the process log files
+#Started by Riley C on 9/20/2023
+echo "To switch between windows use CTRL-B <WINDOW #>"
+echo "To disconnect use CTRL-B d, NOT CTRL-D"
+sleep 2
+tmux new-session -d -s winlinkauto
+tmux new-window -d -n pat -t winlinkauto:1 "bash"
+tmux new-window -d -n rigctl -t winlinkauto:2 "bash"
+tmux new-window -d -n ardop -t winlinkauto:3 "bash"
+tmux new-window -d -n direwolf -t winlinkauto:4 "bash"
+tmux new-window -d -n shell -t winlinkauto:5 "bash"
+tmux send -t winlinkauto:1 "tail -f /var/log/winlinkauto/pat.log" C-m
+tmux send -t winlinkauto:2 "tail -f /var/log/winlinkauto/rigctl.log" C-m
+tmux send -t winlinkauto:3 "tail -f /var/log/winlinkauto/ardop.log" C-m
+tmux send -t winlinkauto:4 "tail -f /var/log/winlinkauto/direwolf.log" C-m
+
+tmux attach -t winlinkauto
