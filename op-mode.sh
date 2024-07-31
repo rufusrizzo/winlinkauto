5e61d16eb04db31c31523f6b9d74507e81ecd126
Riley E. Chandler <riley@netandnix.net>
2024-07-31 08:04:48 -0400

    Another tail improvement

--- op-mode.sh
+++ op-mode.sh
@@ -13,7 +13,8 @@ tmux new-window -d -n shell -t winlinkauto:5 "bash"
 #tmux send -t winlinkauto:1 "tail -f /var/log/winlinkauto/pat.log" C-m
 tmux send -t winlinkauto:1 "multitail -i /var/log/winlinkauto/pat.log -i /home/riley/.local/state/pat/pat.log -i /home/riley/.local/state/pat/eventlog.json" C-m
 tmux send -t winlinkauto:2 "tail -F /var/log/winlinkauto/rigctl.log" C-m
-tmux send -t winlinkauto:3 "tail -F /var/log/winlinkauto/ardop.log" C-m
+#tmux send -t winlinkauto:3 "tail -F /var/log/winlinkauto/ardop.log" C-m
+tmux send -t winlinkauto:3 "multitail -i /var/log/winlinkauto/ardop.log -i /var/log/winlinkauto/ardop/`ls -ltr /var/log/winlinkauto/ardop/ | tail -1 | awk '{print $9}'`" C-m
 tmux send -t winlinkauto:4 "tail -F /var/log/winlinkauto/direwolf.log" C-m
 
 tmux attach -t winlinkauto
