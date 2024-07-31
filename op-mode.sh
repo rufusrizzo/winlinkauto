9f2ecba28b442c4c7d0df8f7b215a8ff5301ef30
Riley E. Chandler <riley@netandnix.net>
2024-07-31 07:53:39 -0400

    updates on log file tails

--- op-mode.sh
+++ op-mode.sh
@@ -11,9 +11,9 @@ tmux new-window -d -n ardop -t winlinkauto:3 "bash"
 tmux new-window -d -n direwolf -t winlinkauto:4 "bash"
 tmux new-window -d -n shell -t winlinkauto:5 "bash"
 #tmux send -t winlinkauto:1 "tail -f /var/log/winlinkauto/pat.log" C-m
-tmux send -t winlinkauto:1 "sudo multitail -i /var/log/winlinkauto/pat.log -i /home/riley/.local/state/pat/pat.log -i /home/riley/.local/state/pat/eventlog.json" C-m
-tmux send -t winlinkauto:2 "tail -f /var/log/winlinkauto/rigctl.log" C-m
-tmux send -t winlinkauto:3 "tail -f /var/log/winlinkauto/ardop.log" C-m
-tmux send -t winlinkauto:4 "tail -f /var/log/winlinkauto/direwolf.log" C-m
+tmux send -t winlinkauto:1 "multitail -i /var/log/winlinkauto/pat.log -i /home/riley/.local/state/pat/pat.log -i /home/riley/.local/state/pat/eventlog.json" C-m
+tmux send -t winlinkauto:2 "tail -F /var/log/winlinkauto/rigctl.log" C-m
+tmux send -t winlinkauto:3 "tail -F /var/log/winlinkauto/ardop.log" C-m
+tmux send -t winlinkauto:4 "tail -F /var/log/winlinkauto/direwolf.log" C-m
 
 tmux attach -t winlinkauto
